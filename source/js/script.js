19e92a5649a5425a11f37e091010b45f0295b34c
Mishasya <Mishasya@bk.ru>
2019-06-08 18:05:03 +0300

    js для мобильного меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
 'use sctrict'
+
+var toggle = document.querySelector(".page-header__toggle");
+var navigation = document.querySelector(".main-navigation");
+
+toggle.addEventListener("click", function () {
+  navigation.classList.toggle("main-navigation--show");
+});
