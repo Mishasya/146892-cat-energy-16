503e28f04b4f02c95ea3e613656517efa44bbdcb
Mishasya <Mishasya@bk.ru>
2019-06-10 16:54:51 +0300

    Трансформация бургера в крестик

--- source/js/script.js
+++ source/js/script.js
@@ -5,4 +5,6 @@ var navigation = document.querySelector(".main-navigation");
 
 toggle.addEventListener("click", function () {
   navigation.classList.toggle("main-navigation--show");
+  toggle.classList.toggle("page-header__toggle--none");
 });
+
